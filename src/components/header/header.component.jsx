0e1b7a4b74af327fa803c6b3cad0f0b1276204db
Eric Schulz <eric.d.schulz@gmail.com>
2020-04-05 14:18:42 -0500

    added user selectors and updated components to use structured selector

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { auth } from '../../firebase/firebase.utils';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import './header.styles.scss';
 
 const Header = ({currentUser, hidden}) => (
@@ -32,15 +35,13 @@ const Header = ({currentUser, hidden}) => (
       }
       <CartIcon />
     </div>
-    {
-      hidden ? null : <CartDropdown />
-    }
+      { hidden ? null : <CartDropdown /> }
   </div>
 );
 
-const mapStateToProps = ({user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser,
+  hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
